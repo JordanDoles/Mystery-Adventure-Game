7ead46ae44342c2eadd03640fcf881a3e5c84ed1
Jordan D <129626792+JordanDoles@users.noreply.github.com>
2026-04-10 13:57:09 -0400

    Create gui.py

---
+++ gui/gui.py
@@ -0,0 +1,21 @@
+# TODO: create main window with Tkinter
+
+# TODO: set window title and size
+
+# TODO: create main layout:
+# - top area (game title / current location)
+# - center area (description / visuals)
+# - side panel (suspects / actions / clues)
+# - bottom (buttons like Investigate, Accuse)
+
+# TODO: create functions to update UI:
+# - update_location_display(location)
+# - update_clue_inventory(clues)
+# - update_suspect_list(suspects)
+
+# TODO: handle button clicks:
+# - investigate_button_clicked()
+# - accuse_button_clicked()
+
+# NOTE:
+# Use placeholder data for now until backend is ready
