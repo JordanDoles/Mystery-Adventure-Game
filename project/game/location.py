393ecfb1a589cbb37884da9a1915b0e8937362bf
Emma Nei <ejnei@icloud.com>
2026-04-10 19:37:17 -0400

    Update location.py

---
+++ project/game/location.py
@@ -0,0 +1,13 @@
+class Location:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def get_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def __str__(self):
+        return f"{self.name} - {self.description}"
