dff3fea06f3e366a46b168050d572a337d73964e
Jordan D <129626792+JordanDoles@users.noreply.github.com>
2026-04-11 13:29:06 -0400

    Update main.py
    removed completed TODOs

--- project/main.py
+++ project/main.py
@@ -1,13 +1,4 @@
-# TODO: Entry point of the program
-
-# TODO: initialize game data / engine (from backend)
-# TODO: initialize GUI
-# TODO: connect GUI to game logic
-# TODO: start main loop (GUI loop)
-
-# Later:
-# - call functions to get_suspects(), get_locations(), get_objects()
-# - pass that data into GUI to display
+# Starts the game
 
 import tkinter as tk
 from gui.gui import MysteryGUI
