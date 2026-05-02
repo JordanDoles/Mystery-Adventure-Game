01f20b1150caf9752288999a92ef13304a0050e7
Emma Nei <ejnei@icloud.com>
2026-05-02 14:25:59 -0400

    2 pytest functions - 1 is giving me error rn test_game.py

---
+++ project/tests/test_game.py
@@ -0,0 +1,30 @@
+import pytest
+from load_data import load_variations_df, load_specific_case_id, load_random_variation, load_random_culprit, load_culprits_df
+
+#checking if variation ids for heirloom are actually broken, stolen and misplaced and not any more 
+def test_variations_for_heirloom():
+    variations_df = load_variations_df()
+    heirloom_variations = []
+    for i, row in variations_df.iterrows():
+        if row["case_id"] == "heirloom":
+            heirloom_variations.append(row["variation_id"])
+
+    assert "broken" in heirloom_variations
+    assert "stolen" in heirloom_variations
+    assert "misplaced" in heirloom_variations
+    assert len(heirloom_variations) == 3
+
+#trying to check that case id, variation and culprit that used in load_data matches the values in df - giving me error atm about postional arguments 
+def test_culprit_matches_case_and_variation_in_df():
+    caseid = load_specific_case_id("heirloom")
+    variation = load_random_variation(caseid.get_case_id())
+    culprit = load_random_culprit(caseid.get_case_id(), variation.get_variation_id())
+
+    culprits_df = load_culprits_df()
+    match = False
+    for i, row in culprits_df.iterrows():
+        if (row["case_id"] == caseid.get_case_id() and
+            row["variation_id"] == variation.get_variation_id() and
+            row["culprit_id"] == culprit.get_culprit_id()):
+            match = True
+    assert match
