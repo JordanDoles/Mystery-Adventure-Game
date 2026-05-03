da748223b45e4444395086aa5559812b340c2267
Emma Nei <ejnei@icloud.com>
2026-05-02 21:45:29 -0400

    Updated docstrings

--- project/game/case.py
+++ project/game/case.py
@@ -1,29 +1,59 @@
 class Case:
-    def __init__(self, case_id, name, opening, what_choice_1, what_choice_2, what_choice_3):
+    """
+    Case Class
+    """
+    def __init__(self, case_id, case_name, what_choice_1, what_choice_2, what_choice_3):
+        """
+        Provides case for given case_id and case_name - case object initialization
+        :param case_id: case id
+        :type case_id: str
+        :param case_name: case name
+        :type case_name: str
+        :param what_choice_1: choice possible
+        :type what_choice_1: str
+        :param what_choice_2: 2nd choice possible
+        :type what_choice_2: str
+        :param what_choice_3: 3rd choice possible
+        :type what_choice_3: str
+        :return: None
+        """
         self.__case_id = case_id
-        self.__name = name
-        self.__opening = opening
+        self.__case_name = case_name
         self.__what_choice_1 = what_choice_1
         self.__what_choice_2 = what_choice_2
         self.__what_choice_3 = what_choice_3
 
     def get_case_id(self):
+        """
+        Gets case id
+        :return: case id
+        """
         return self.__case_id
 
     def get_name(self):
-        return self.__name
+        """
+        Gets case name
+        :return: case name
+        """
+        return self.__case_name
 
-    def set_name(self, name):
-        self.__name = name
-
-    def get_opening(self):
-        return self.__opening
-    
     def get_what_choice_1(self):
+        """
+        Gets choice possible
+        :return: choice 1
+        """
         return self.__what_choice_1
-        
+
     def get_what_choice_2(self):
+        """
+        Gets 2nd choice possible
+        :return: choice 2 
+        """
         return self.__what_choice_2
-        
+
     def get_what_choice_3(self):
+        """
+        Gets 3rd choice possible
+        :return: choice 3
+        """
         return self.__what_choice_3
