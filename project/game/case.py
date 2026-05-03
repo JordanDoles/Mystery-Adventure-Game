41a27c4acd1300b77a59c7abb171b82cc006f9bd
Emma Nei <ejnei@icloud.com>
2026-05-02 22:05:09 -0400

    Update case.py

--- project/game/case.py
+++ project/game/case.py
@@ -2,13 +2,15 @@ class Case:
     """
     Case Class
     """
-    def __init__(self, case_id, case_name, what_choice_1, what_choice_2, what_choice_3):
+    def __init__(self, case_id, case_name, case_opening, what_choice_1, what_choice_2, what_choice_3):
         """
         Provides case for given case_id and case_name - case object initialization
         :param case_id: case id
         :type case_id: str
         :param case_name: case name
         :type case_name: str
+        :param case_opening: case opening
+        :type case_opening: str
         :param what_choice_1: choice possible
         :type what_choice_1: str
         :param what_choice_2: 2nd choice possible
@@ -19,6 +21,7 @@ class Case:
         """
         self.__case_id = case_id
         self.__case_name = case_name
+        self.__case_opening = case_opening
         self.__what_choice_1 = what_choice_1
         self.__what_choice_2 = what_choice_2
         self.__what_choice_3 = what_choice_3
@@ -37,6 +40,13 @@ class Case:
         """
         return self.__case_name
 
+    def get_opening(self):
+        """
+        Gets case opening
+        :return: case opening
+        """
+        return self.__case_opening
+
     def get_what_choice_1(self):
         """
         Gets choice possible
@@ -47,7 +57,7 @@ class Case:
     def get_what_choice_2(self):
         """
         Gets 2nd choice possible
-        :return: choice 2 
+        :return: choice 2
         """
         return self.__what_choice_2
 
@@ -57,3 +67,4 @@ class Case:
         :return: choice 3
         """
         return self.__what_choice_3
+
