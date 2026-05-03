098cb088336356a7ba195afebf3f516afc325308
Emma Nei <ejnei@icloud.com>
2026-05-02 23:29:42 -0400

    Update suspect.py

--- project/game/suspect.py
+++ project/game/suspect.py
@@ -1,19 +1,48 @@
 class Suspect:
-    def __init__(self, suspect_id, name, role, description):
+    """
+    Suspect class
+    """
+    def __init__(self, suspect_id, suspect_name, suspect_role, suspect_description):
+        """
+        Initialize a Suspect object
+        :param suspect_id: suspect id
+        :type suspect_id: str
+        :param suspect_name: name of the suspect
+        :type suspect_name: str
+        :param suspect_role: role of the suspect
+        :type suspect_role: str
+        :param suspect_description: description of the suspect
+        """
         self.__suspect_id = suspect_id
-        self.__name = name
-        self.__role = role
-        self.__description = description
+        self.__suspect_name = suspect_name
+        self.__suspect_role = suspect_role
+        self.__suspect_description = suspect_description
 
     # Getters
     def get_suspect_id(self):
+        """
+        Get the suspect id
+        :return: suspect id
+        """
         return self.__suspect_id
 
     def get_name(self):
-        return self.__name
+        """
+        Get the suspect name
+        :return: name of the suspect
+        """
+        return self.__suspect_name
 
     def get_role(self):
-        return self.__role
+        """
+        Get the role of the suspect
+        :return: role of the suspect
+        """
+        return self.__suspect_role
 
     def get_description(self):
-        return self.__description
+        """
+        get the description of the suspect
+        :return: description of the suspect
+        """
+        return self.__suspect_description
